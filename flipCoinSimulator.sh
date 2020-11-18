4e4538884bca09c9617fc5b0cafbd0a8270ee1cf
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-18 18:53:38 +0530

    Added: Random flipcoin logic

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,11 @@
 echo "Welcome of the flip coin simulation program being solved in the Master branch"
+random=$((RANDOM%2))
+echo "$random"
+if(($random==1))
+then
+     echo "Heads"
+     echo "winner"
+else
+    echo "Tails"
+    echo "looser"
+fi
